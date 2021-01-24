8d6b414f0cd3691fd631125fbe2d2c248edfd136
Shilpi <Shilpi@DESKTOP-CHN255S>
2021-01-24 23:10:39 +0000

    commit

--- CakeManager/src/main/java/com/cake/manager/ManagerApplication.java
+++ CakeManager/src/main/java/com/cake/manager/ManagerApplication.java
@@ -2,11 +2,8 @@ package com.cake.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
-//@EnableOAuth2Sso
 public class ManagerApplication {
 
 	public static void main(String[] args) {
